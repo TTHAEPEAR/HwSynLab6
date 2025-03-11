51ab10aa355dd72094638f10c3162f5a866140ce
TTHAEPEAR <101453081+TTHAEPEAR@users.noreply.github.com>
2025-03-11 14:24:49 +0700

    Update BranchComp.v

--- BranchComp.v
+++ BranchComp.v
@@ -1,12 +1,13 @@
 module BranchComp(
-    input [31:0] rs1,       // First register value
-    input [31:0] rs2,       // Second register value
-    output brLt,            // Output for less than condition
-    output brEq             // Output for equality condition
+    input signed [31:0] rs1,   // First register value (signed)
+    input signed [31:0] rs2,   // Second register value (signed)
+    output brLt,               // Output for less than condition
+    output brEq                // Output for equality condition
 );
+    // Implement branch comparator for checking if value in register
+    // is less than or equal to another register
 
-    // TODO: implement your branch comparator here for checking if
-    // value is register is less than or equal to another register
-
+    assign brEq = (rs1 == rs2);  // Check for equality
+    assign brLt = (rs1 < rs2);   // Check for less than (signed)
 
 endmodule
